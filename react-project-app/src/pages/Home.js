571067ce3a1895465c33076374a853bf03e83a39
pradeepnegi8979 <165800314+pradeepnegi8979@users.noreply.github.com>
2024-04-03 13:29:37 +0530

    Update Home.js

--- react-project-app/src/pages/Home.js
+++ react-project-app/src/pages/Home.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { Header } from "../common/Header";
 import { Footer } from "../common/Footer";
-
-import { Todo } from "../Todo";
+import { Container, Row, Col } from "react-bootstrap";
 function Home() {
   return (
     <>
       <Header />
-      <Todo />
-
+      <section className="homepage">
+        <Container>
+          <Row>
+            <Col>Home Page</Col>
+          </Row>
+        </Container>
+      </section>
       <Footer />
     </>
   );
