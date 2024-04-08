9cdce63b07310214ebba580ded5e64d6ac4d92be
pradeepnegi8979 <165800314+pradeepnegi8979@users.noreply.github.com>
2024-04-08 12:07:11 +0530

    Update Services.js

--- react-project-app/src/pages/Services.js
+++ react-project-app/src/pages/Services.js
@@ -1,18 +1,12 @@
 import React from "react";
 import { Header } from "../common/Header";
 import { Footer } from "../common/Footer";
-import { Container, Row, Col } from "react-bootstrap";
+import { Tabs } from "../Tabs";
 function Services() {
   return (
     <>
       <Header />
-      <section className="servicesSection">
-        <Container>
-          <Row>
-            <Col md="12">services page</Col>
-          </Row>
-        </Container>
-      </section>
+      <Tabs />
       <Footer />
     </>
   );
